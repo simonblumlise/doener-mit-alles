6de2ef9180c2cb19bcf1f7c91df820ecac3c34ea
Tobias Grunwald <tobias.grunwald@lise.de>
2022-12-02 14:25:18 +0100

    add template api

---
+++ api/src/main/kotlin/de/lise/doenermitalles/DoenermitallesApplication.kt
@@ -0,0 +1,11 @@
+package de.lise.doenermitalles
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DoenermitallesApplication
+
+fun main(args: Array<String>) {
+	runApplication<DoenermitallesApplication>(*args)
+}
---
+++ api/src/test/kotlin/de/lise/doenermitalles/DoenermitallesApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.lise.doenermitalles
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DoenermitallesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
